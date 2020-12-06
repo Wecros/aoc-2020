e3f8d630ad7de5c457e6cc831d9d16609f91a348
wecros <wecros@gmail.com>
2020-12-06 18:25:11 +0100

    Remove blank lines in the day 4 solution

--- day4/part2.py
+++ day4/part2.py
@@ -19,13 +19,10 @@ def validate_data(field: str, passport: dict) -> bool:
 
     if field == 'byr':
         return year >= 1920 and year <= 2002
-
     elif field == 'iyr':
         return year >= 2010 and year <= 2020
-
     elif field == 'eyr':
         return year >= 2020 and year <= 2030
-
     elif field == 'hgt':
         matched = re.match('([0-9]*)(cm|in)', value)
         if not matched:
@@ -36,19 +33,15 @@ def validate_data(field: str, passport: dict) -> bool:
         if unit == 'cm':
             return height >= 150 and height <= 193
         return height >= 59 and height <= 76
-
     elif field == 'hcl':
         matched = re.match('#[0-9a-f]{6}', value)
         if not matched:
             return False
         return True
-
     elif field == 'ecl':
         return value in ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth')
-
     elif field == 'pid':
         return len(value) == 9 and value.isnumeric()
-
     else:
         return False
 
